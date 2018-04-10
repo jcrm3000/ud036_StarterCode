33ed404aeb78a5cd0fab2f05e889af52b5f9e68f
Juan Carlos Romero Marcano <jcrm3000@Juans-MacBook-Pro.local>
2018-04-09 22:30:18 -0600

    Creating a child class called "Movie"
    Importing the library "webbrowser" to be able to use the "open" class
    
    Creating a class method in the child class "Movie" to be able to show trailers

--- media.py
+++ media.py
@@ -1,18 +1,36 @@
+#Importing the library "webbrowser" to be able to use the "open" class
+import webbrowser
 
 
 
-
-#Creating a class called Movie
+#Creating a parent class
 
 class Video:
     
-    #Creating the constructor of the class and puting the atributtes to it
+    #Creating the constructor of the class and puting the attributes to it
 
     def __init__(self, title, duration):
 
-        #Initializing the properties variables
+        #Initializing the attributes variables
         
         self.title = title
         self.duration = duration
+
+#Creating a child class
+class Movie(Video):
+
+    #Creating the constructor of the class and puting the attributes to it
+    def __init__(self, title, duration, storyline, poster_image,
+                 trailer_youtube):
+        Video.__init__(self, title, duration)
+        self.storyline = storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+    #Creating a class method to be able to show trailers
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+
+
+        
 
 
