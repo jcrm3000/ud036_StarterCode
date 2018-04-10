0f1552b5c152b710b4a353cafb9c08cab2d81c90
Juan Carlos Romero Marcano <jcrm3000@Juans-MacBook-Pro.local>
2018-04-09 23:35:21 -0600

    creating three objects of the class "Movie" and two objects of the class "Tv_show"

---
+++ entertaiment_center.py
@@ -0,0 +1,29 @@
+import media
+
+
+#Creating an object of the class "Movie"
+toy_story = media.Movie("Toy story",str(2)+" horas",
+                           "Toys that are alive",
+                           "goo.gl/2aVBEy","goo.gl/kS9eph")
+
+#Creating an object of the class "Movie"
+avatar = media.Movie("Avatar",str(2)+" horas",
+                        "A guy on an alien planet",
+                        "goo.gl/89dzc8","goo.gl/gwsavy")
+
+#Creating an object of the class "Movie"
+interstellar = media.Movie("Interstellar",str(2)+" horas",
+                              "A guy, who goes into the space",
+                              "goo.gl/zUuZ2x","goo.gl/wUW7zT")
+
+#Creating an object of the class "Tv_show"
+breaking_bad = media.Movie("Breaking bad",str(100)+" horas",
+                              "A professor, how makes drogs",
+                              "goo.gl/7ZdQT1","goo.gl/CFhcgP",
+                               5,62,"RTL")
+
+#Creating an object of the class "Tv_show"
+game_of_thrones = media.Movie("Game of thrones",str(100)+" horas",
+                              "People fighting for the iron throne",
+                              "goo.gl/j7zoqm","goo.gl/KWcbn4",
+                               8,60,"HBO")
