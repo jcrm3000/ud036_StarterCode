02aa4791752304fa8e6bda7891acdca8bbe8fe24
Juan Carlos Romero Marcano <jcrm3000@Juans-MacBook-Pro.local>
2018-04-09 23:16:11 -0600

    Creating a child class called "Tv_show" of the class "Movie" being it the grandchild of the class "Video"

--- media.py
+++ media.py
@@ -1,10 +1,7 @@
-#Importing the library "webbrowser" to be able to use the "open" class
 import webbrowser
 
 
-
 #Creating a parent class
-
 class Video:
     
     #Creating the constructor of the class and puting the attributes to it
@@ -16,21 +13,36 @@ class Video:
         self.title = title
         self.duration = duration
 
-#Creating a child class
+
+#Creating a child class of the class "Video"
 class Movie(Video):
 
     #Creating the constructor of the class and puting the attributes to it
     def __init__(self, title, duration, storyline, poster_image,
                  trailer_youtube):
+        
         Video.__init__(self, title, duration)
         self.storyline = storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+
     #Creating a class method to be able to show trailers
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
 
 
+#Creating a child class of the class "Movie"
+#being the grandchild of the class "Video"
+class Tv_show(Movie):
+
+    def __init__(self, title, duration, storyline,
+                 poster_image, trailer_youtube,
+                 season, episode, tv_station):
         
+        Movie.__init__(self, title, duration, storyline,
+                       poster_image, trailer_youtube)
+        self.season = season
+        self.episode = episode
+        self.tv_station = tv_station
 
 
