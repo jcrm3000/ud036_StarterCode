fef67c295a704b360ba3fd286091a4e27645d870
Juan Carlos Romero Marcano <jcrm3000@Juans-MacBook-Pro.local>
2018-04-10 00:16:53 -0600

    Making the given urls of the attributes "poster_image" shorter with https://bitly.com/ and puting as the urls for the "trailer_youtube" the original urls because they are not so long

--- entertaiment_center.py
+++ entertaiment_center.py
@@ -3,30 +3,35 @@ import fresh_tomatoes
 
 #Creating an object of the class "Movie"
 toy_story = media.Movie("Toy story",str(2)+" horas",
-                           "Toys that are alive",
-                           "goo.gl/2aVBEy","goo.gl/kS9eph")
+                        "Toys that are alive",
+                        "https://bit.ly/2GKm3Jn",
+                        "https://www.youtube.com/watch?v=ZZv1vki4ou4")
 
 #Creating an object of the class "Movie"
 avatar = media.Movie("Avatar",str(2)+" horas",
-                        "A guy on an alien planet",
-                        "goo.gl/89dzc8","goo.gl/gwsavy")
+                     "A guy on an alien planet",
+                     "https://bit.ly/2Exvhmn",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 #Creating an object of the class "Movie"
 interstellar = media.Movie("Interstellar",str(2)+" horas",
-                              "A guy, who goes into the space",
-                              "goo.gl/zUuZ2x","goo.gl/wUW7zT")
+                           "A guy, who goes into the space",
+                           "https://bit.ly/2JzknjN",
+                           "https://www.youtube.com/watch?v=zSWdZVtXT7E")
 
 #Creating an object of the class "Tv_show"
 breaking_bad = media.Tv_show("Breaking bad",str(100)+" horas",
                               "A professor, how makes drogs",
-                              "goo.gl/7ZdQT1","goo.gl/CFhcgP",
+                              "https://bit.ly/2mC5DE8",
+                              "https://www.youtube.com/watch?v=rJnjxvgvkBM",
                                5,62,"RTL")
 
 #Creating an object of the class "Tv_show"
 game_of_thrones = media.Tv_show("Game of thrones",str(100)+" horas",
-                              "People fighting for the iron throne",
-                              "goo.gl/j7zoqm","goo.gl/KWcbn4",
-                               8,60,"HBO")
+                                "People fighting for the iron throne",
+                                "https://bit.ly/2qfYvlq",
+                                "https://www.youtube.com/watch?v=v5gsVRxzzI4",
+                                8,60,"HBO")
 
 movies = [toy_story, avatar, interstellar, breaking_bad, game_of_thrones]
 
